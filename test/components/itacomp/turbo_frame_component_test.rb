6f0c15ac17de5b297abd596dd815afffdd035359
MDreW <andrea.ranaldi@gmail.com>
2025-08-22 12:30:43 +0200

    Prefer double-quoted strings unless you need single quotes

--- test/components/itacomp/turbo_frame_component_test.rb
+++ test/components/itacomp/turbo_frame_component_test.rb
@@ -10,16 +10,16 @@ class Itacomp::TurboFrameComponentTest < ViewComponent::TestCase
 
   test "content go in turbo-frame tag" do
     render_inline Itacomp::TurboFrameComponent.new().with_content("test")
-    assert_selector "turbo-frame", text: 'test'
+    assert_selector "turbo-frame", text: "test"
   end
 
   test "any keys is added to turbo-frame tag" do
-    render_inline Itacomp::TurboFrameComponent.new(id: 'test', class: 'tost')
+    render_inline Itacomp::TurboFrameComponent.new(id: "test", class: "tost")
     assert_selector "turbo-frame#test.tost", text: nil
   end
 
   test "if is present href key and content is empty is added the loader div" do
-    render_inline Itacomp::TurboFrameComponent.new(href: 'http://test.it')
-    assert_selector "turbo-frame div.progress-spinner.progress-spinner-active span.visually-hidden", text: 'Loading...'
+    render_inline Itacomp::TurboFrameComponent.new(href: "http://test.it")
+    assert_selector "turbo-frame div.progress-spinner.progress-spinner-active span.visually-hidden", text: "Loading..."
   end
 end
--- test/components/previews/itacomp/turbo_frame_component_preview.rb
+++ test/components/previews/itacomp/turbo_frame_component_preview.rb
@@ -3,19 +3,19 @@
 module Itacomp
   class TurboFrameComponentPreview < ViewComponent::Preview
     def default
-      render TurboFrameComponent.new(id: 'text').with_content('text content')
+      render TurboFrameComponent.new(id: "text").with_content("text content")
     end
 
     def empty
-      render TurboFrameComponent.new(id: 'empty')
+      render TurboFrameComponent.new(id: "empty")
     end
 
     def remote
-      render TurboFrameComponent.new(id: 'remote', href: 'http://itacomp.it')
+      render TurboFrameComponent.new(id: "remote", href: "http://itacomp.it")
     end
 
     def remote_with_content
-      render TurboFrameComponent.new(id: 'remote-with-content', href: 'http://itacomp.it').with_content('text content')
+      render TurboFrameComponent.new(id: "remote-with-content", href: "http://itacomp.it").with_content("text content")
     end
   end
 end
