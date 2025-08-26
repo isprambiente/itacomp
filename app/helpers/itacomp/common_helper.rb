09e9842f71a610f865f406fa2e77474231b0425a
MDreW <andrea.ranaldi@gmail.com>
2025-08-26 16:46:23 +0200

    add ita-spinner helper and revrited helper docs

--- app/helpers/itacomp/common_helper.rb
+++ app/helpers/itacomp/common_helper.rb
@@ -1,32 +1,79 @@
 module Itacomp
   # Common helper for itacomp component
-  # To add these helerp in your rails projec add `include Itacomp::CommonHelper` in yout `ApplicationHelper`
+  # To add these helerp in your rails projec add <tt>include Itacomp::CommonHelper</tt> in yout <tt>ApplicationHelper</tt>
   module CommonHelper
-    # @example ita_visually_hidden
-    #  <span class="visually-hidden"></span>
+    # return an html span tag with class <tt>visually-hidden</tt> and an optional text
     #
-    # @example ita_visually_hidden('text')
-    #   <span class="visually-hidden">text</span>
+    # ==== Options
+    # * <tt>content</tt> [String] default <tt>nil</tt>
     #
-    # @param [String] content Optiona html/string content
-    #
-    # @return [String] html span tag with class `visually-hidden` and optional content
+    # ==== Example
+    # Without options
+    #   ita_visually_hidden
+    #   # => <span class="visually-hidden"></span>
+    # With text
+    #   ita_visually_hidden('text')
+    #   # => <span class="visually-hidden">text</span>
     def ita_visually_hidden(content = nil)
       tag.span content, class: "visually-hidden"
     end
 
-    # @example icon('test')
-    #  "<svg class="icon"><use href="/itacomp/sprites.svg#test" xlink:href="/itacomp/sprites.svg#test" /></svg>"
+    # Return html structure for an {icon}[https://italia.github.io/bootstrap-italia/docs/utilities/icone/] 
     #
-    # @example icon('test', class: 'icon align-bottom', id: 'ok')
-    #  "<svg class="icon align-bottom" id="ok""><use href="/itacomp/sprites.svg#test" xlink:href="/itacomp/sprites.svg#test" /></svg>"
+    # ==== Options
+    # * <tt>icon</tt> [String] no default (required), icon svg id
+    # * <tt>:sprites</tt> [String] default <tt>'itacomp/sprites.svg'<.tt>, sprites asset path, default is sprites shared from Itacomp
+    # * <tt>class</tt> [String,Array] default <tt>nil</tt> if present is added ad tag class after <tt>icon</tt>
+    # * <tt>**opts</tt> each key is delegated as tag options
     #
-    # @param [String] icon - icon id for sprites.svg
-    # @param [String] :sprites sprites file path for asset_path, default 'itacomp/sprites.svg' from Itacomp asset
-    # @param [String] :class SVG tag class, default 'icon'
-    def ita_icon(icon, sprites: "itacomp/sprites.svg", class: "icon", **opts)
+    # ==== Examples
+    # Simple icon
+    #   icon('test')
+    #   # => "<svg class="icon"><use href="/itacomp/sprites.svg#test" xlink:href="/itacomp/sprites.svg#test" /></svg>"
+    #
+    # With additional <tt>class</tt> and <tt>id</tt>
+    #   icon('test', class: 'align-bottom', id: 'ok')
+    #   # => "<svg class="icon align-bottom" id="ok""><use href="/itacomp/sprites.svg#test" xlink:href="/itacomp/sprites.svg#test" /></svg>"
+    def ita_icon(icon, sprites: "itacomp/sprites.svg", **opts)
       url = asset_path("#{sprites}##{icon}")
-      tag.svg tag.use(href: url, "xlink:href": url),  class:, **opts
+      opts[:class] = ['icon', opts[:class]]
+      tag.svg tag.use(href: url, "xlink:href": url),  **opts
+    end
+
+    # return html for a {spinner component}[https://italia.github.io/bootstrap-italia/docs/componenti/progress-indicators/#spinner]
+    #
+    # ==== Options
+    #
+    # * <tt>content</tt> [String] default <tt>nil</tt>, text content in visually hidden tag
+    # * <tt>:active</tt> [Boolean] default <tt>true</tt>, if true add <tt>progress-spinner-active</tt> class
+    # * <tt>:double</tt> [Boolean] default <tt>false</tt>, if active add <tt>progress-spinner-double</tt> class 
+    # * <tt>:class</tt> [String,Array] default <tt>nil</tt>, if present is added as tag class after <tt>progress-spinner</tt>
+    # * <tt>**opts</tt> each key is delegated as tag options
+    #
+    # ==== example
+    # Without options (default active)
+    #   ita_spinner()
+    #   # => <div class="progress-spinner progress-spinner-active"><span class="visually-hidden"></span></div>
+    # Inactive spinner
+    #   ita_spinner(active: false)
+    #   # => <div class="progress-spinner"><span class="visually-hidden"></span></div>
+    # With text in span.visually-hidden tag
+    #   ita_spinner('Loading')
+    #   # => <div class="progress-spinner progress-spinner-active"><span class="visually-hidden">Loading</span></div>
+    # Double With text in span.visually-hidden tag
+    #   ita_spinner('Loading', double: true)
+    #   # => <div class="progress-spinner progress-spinner-active progress-spinner-double"><span class="visually-hidden">Loading</span></div>
+    # With additional class and text in span.visually-hidden tag
+    #   ita_spinner('Loading', class: 'size-xl')
+    #   # => <div class="progress-spinner progress-spinner-active size-xl"><span class="visually-hidden">Loading</span></div>
+    # With additional class, id attribute, data attribute abd text in span.visually-hidden tag 
+    #   ita_spinner('Loading', class: 'size-xl', id: 'my-spinner', data-load: 'my-loader')
+    #   # => <div id="my-spinner" class="progress-spinner progress-spinner-active size-xl" data-load="my-loader"><span class="visually-hidden">Loading</span></div>
+    def ita_spinner(content = nil, active: true, double: false, **opts)
+      opts[:class] = ['progress-spinner', opts[:class]]
+      opts[:class] << 'progress-spinner-double' if double
+      opts[:class] << "progress-spinner-active" if active
+      tag.div ita_visually_hidden(content), **opts
     end
   end
 end
--- test/helpers/common_helper_test.rb
+++ test/helpers/common_helper_test.rb
@@ -9,6 +9,15 @@ class CommonHelperTest < ActionView::TestCase
 
   test "ita_icon test" do
     assert_equal "<svg class=\"icon\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test")
-    assert_equal "<svg class=\"icon icon-bg-top\" id=\"tost\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test", class: "icon icon-bg-top", id: "tost")
+    assert_equal "<svg class=\"icon icon-bg-top\" id=\"tost\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test", class: "icon-bg-top", id: "tost")
+  end
+
+  test "ita_spinner" do
+    assert_equal '<div class="progress-spinner progress-spinner-active"><span class="visually-hidden"></span></div>', ita_spinner
+    assert_equal '<div class="progress-spinner"><span class="visually-hidden"></span></div>', ita_spinner(active: false)
+    assert_equal '<div class="progress-spinner progress-spinner-active"><span class="visually-hidden">Loading</span></div>', ita_spinner('Loading')
+    assert_equal '<div class="progress-spinner progress-spinner-double progress-spinner-active "><span class="visually-hidden">Loading</span></div>', ita_spinner('Loading', double: true)
+    assert_equal '<div class="progress-spinner progress-spinner-active size-xl"><span class="visually-hidden">Loading</span></div>', spinner('Loading', class: 'size-xl')
+    assert_equal '<div id="my-spinner" class="progress-spinner progress-spinner-active size-xl" data-load="my-loader"><span class="visually-hidden">Loading</span></div>', ita_spinner('Loading', class: 'size-xl', id: 'my-spinner', data: {load: 'my-loader'})
   end
 end
