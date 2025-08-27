8b6fa7180ea27c45a20f7ef22bdc3b0bdd252dcd
MDreW <andrea.ranaldi@gmail.com>
2025-08-27 17:18:29 +0200

    type of bootstrap-italia moved in base module

--- lib/itacomp.rb
+++ lib/itacomp.rb
@@ -2,5 +2,6 @@ require "itacomp/version"
 require "itacomp/engine"
 
 module Itacomp
-  # Your code goes here...
+  # this constant define available bootstrap-italia type
+  ITA_TYPES = Hash.new { :primary }.with_indifferent_access.merge(primary: "primary", info: "info", success: "success", warning: "warning", danger: "danger")
 end
