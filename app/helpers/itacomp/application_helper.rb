d17a261ae4470515ee890c88c990fee9725896f1
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2025-08-27 17:28:18 +0200

    21 components progress indicators (#27)
    * add ita-spinner helper and revrited helper docs
    
    * Added istruction for demo app
    
    * fix aler doc
    
    * type of bootstrap-italia moved in base module
    
    * addedd ita_progress helper with doc and test

--- app/helpers/itacomp/application_helper.rb
+++ app/helpers/itacomp/application_helper.rb
@@ -1,7 +1,5 @@
 module Itacomp
   # shared data fot all Itacomp Helpers
   module ApplicationHelper
-    # this constant define available bootstrap-italia type
-    ITA_TYPES = Hash.new { :primary }.with_indifferent_access.merge(primary: "primary", info: "info", success: "success", warning: "warning", danger: "danger")
   end
 end
--- test/helpers/common_helper_test.rb
+++ test/helpers/common_helper_test.rb
@@ -9,6 +9,27 @@ class CommonHelperTest < ActionView::TestCase
 
   test "ita_icon test" do
     assert_equal "<svg class=\"icon\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test")
-    assert_equal "<svg class=\"icon icon-bg-top\" id=\"tost\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test", class: "icon icon-bg-top", id: "tost")
+    assert_equal "<svg class=\"icon icon-bg-top\" id=\"tost\"><use href=\"/itacomp/sprites.svg#test\" xlink:href=\"/itacomp/sprites.svg#test\" /></svg>", ita_icon("test", class: "icon-bg-top", id: "tost")
+  end
+
+  test "ita_spinner" do
+    assert_equal '<div class="progress-spinner progress-spinner-active"><span class="visually-hidden"></span></div>', ita_spinner
+    assert_equal '<div class="progress-spinner"><span class="visually-hidden"></span></div>', ita_spinner(active: false)
+    assert_equal '<div class="progress-spinner progress-spinner-active"><span class="visually-hidden">Loading</span></div>', ita_spinner("Loading")
+    assert_equal '<div class="progress-spinner progress-spinner-double progress-spinner-active"><span class="visually-hidden">Loading</span></div>', ita_spinner("Loading", double: true)
+    assert_equal '<div class="progress-spinner size-xl progress-spinner-active"><span class="visually-hidden">Loading</span></div>', ita_spinner("Loading", class: "size-xl")
+    assert_equal '<div class="progress-spinner size-xl progress-spinner-active" id="my-spinner" data-load="my-loader"><span class="visually-hidden">Loading</span></div>', ita_spinner("Loading", class: "size-xl", id: "my-spinner", data: { load: "my-loader" })
+  end
+
+  test "ita_donut" do
+    assert_equal "<div class=\"progress-donut-wrapper\"><div class=\"progress-donut\" data-bs-progress-donut=\"true\" data-bs-value=\"0.0\"></div><span class=\"visually-hidden\">0%</span></div>", ita_donut
+    assert_equal "<div class=\"progress-donut-wrapper\"><div class=\"progress-donut\" data-bs-progress-donut=\"true\" data-bs-value=\"0.5\"></div><span class=\"visually-hidden\">50%</span></div>", ita_donut(50)
+    assert_equal "<div class=\"progress-donut-wrapper\"><div id=\"test\" class=\"progress-donut\" data-bs-progress-donut=\"true\" data-bs-value=\"0.5\"></div><span class=\"visually-hidden\">50%</span></div>", ita_donut(50, id: "test")
+  end
+
+  test "ita_progress" do
+    assert_equal '<div class="progress progress-indeterminate"><div class="progress-bar" role="progressbar"></div></div>', ita_progress
+    assert_equal '<div class="progress"><div class="progress-bar" role="progressbar" style="width: 50%" aria-valuemin="0" aria-valuemax="100" aria-valuenow="50"></div></div>', ita_progress(50)
+    assert_equal '<div class="progress progress-color"><div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuemin="0" aria-valuemax="100" aria-valuenow="50"></div></div>', ita_progress(50, type: "primary")
   end
 end
