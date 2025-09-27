de341ace035cf9c74d1a673af4f16c3576ccbca4
MDreW <andrea.ranaldi@gmail.com>
2025-09-27 19:42:26 +0200

    Rubocop fix

--- test/components/itacomp/tab_component_test.rb
+++ test/components/itacomp/tab_component_test.rb
@@ -9,12 +9,12 @@ class Itacomp::TabComponentTest < ViewComponent::TestCase
   end
 
   test "can add options to ul selector" do
-    render_inline Itacomp::TabComponent.new(id: 'uno', class: 'due', data: {value: 'tre'})
-    assert_selector "ul#uno.nav.nav-tabs.due[role='tablist'][data-value='tre']", text: nil   
+    render_inline Itacomp::TabComponent.new(id: "uno", class: "due", data: { value: "tre" })
+    assert_selector "ul#uno.nav.nav-tabs.due[role='tablist'][data-value='tre']", text: nil
   end
 
   test "can add content as array" do
-    render_inline Itacomp::TabComponent.new([['uno', '/due', {active: true}],['tre','/quattro']])
+    render_inline Itacomp::TabComponent.new([ [ "uno", "/due", { active: true } ], [ "tre", "/quattro" ] ])
     assert_selector "ul.nav.nav-tabs[role='tablist'] li.nav-item a.nav-link[active='true'][href='/due']", text: "uno"
     assert_selector "ul.nav.nav-tabs[role='tablist'] li.nav-item a.nav-link[href='/quattro']", text: "tre"
   end
